e071fe002f7bdbfeda4e2b98a4a899d3d0f3abcd
daniel <daniel870217@gmail.com>
2023-07-22 02:44:55 +0800

    fix: fix deploy setting

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
+  base: "shopping_web_frontend/",
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
